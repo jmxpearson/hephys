e8edf2adb70718180852a846ad944c2876cd7d7e
John Pearson <jmxpearson@gmail.com>
2013-11-16 08:33:31 -0500

    code now correctly builds spikes database

--- build_db.py
+++ build_db.py
@@ -4,50 +4,86 @@ import pandas.io.sql as pdsql
 import scipy.io as sio
 import numpy as np
 
-# connect to database
-db = MySQLdb.connect(host='localhost',
-    user='root', passwd='', db='bartc')
+def SetupDB():
+    # connect to database server
+    db = MySQLdb.connect(host='localhost',
+        user='root', passwd='')
 
-# create cursor
-cur = db.cursor()
+    # create cursor
+    cur = db.cursor()
 
-# make a test query
-qstr = """
-    SELECT * FROM spikes WHERE time < 1 
-    """
+    # create database and switch to it
+    setupstr = """
+        CREATE DATABASE bartc;
+        USE bartc;
+        CREATE TABLE spikes (patient TINYINT, dataset TINYINT, 
+            channel TINYINT, unit TINYINT, time DECIMAL(7,3));
+        """
+    cur.execute(setupstr)
+
+def ImportSpikes(ftup, datadir):
+    pdir = 'patient' + str(ftup[0]).zfill(3)
+    fname = ('times_' + str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + 
+        str(ftup[2]) + '.mat')
+    fullname = datadir + pdir + '/' + fname
+
+    dat = sio.loadmat(fullname)['cluster_class']
+    
+    unit = dat[:,0].astype('int')
+    times = np.around(dat[:,1]/1000., decimals=3) #times are originally in ms
+    sortord = np.argsort(times) #spikes aren't always time-sorted
+    times = times[sortord]
+    unit = unit[sortord]
+    
+    # now restrict to valid units:
+    valid = unit == ftup[3]
+    times = times[valid]
+    unit = unit[valid]
+
+    ddict = {'patient': ftup[0], 'dataset': ftup[1], 
+    'channel': ftup[2], 'unit': unit, 'time': times}
+    df = pd.DataFrame(ddict)
+
+    db = MySQLdb.connect(host='localhost',
+        user='root', passwd='', db='bartc')
+    pdsql.write_frame(df, con=db, name='spikes',
+        if_exists='append', flavor='mysql')
+    db.close()
+
 
 # read data
-dat = pdsql.read_frame(qstr, db)
+# dat = pdsql.read_frame(qstr, db)
 
-# write data
-pdsql.write_frame(dat, con=db, name='supp',
-    if_exists='replace', flavor='mysql')
+# # write data
+# pdsql.write_frame(dat, con=db, name='supp',
+#     if_exists='replace', flavor='mysql')
 
 ########### tests ##################
-patient = 18
-dset = 1
-chan = 1
-unit = 2
+# patient = 18
+# dset = 1
+# chan = 1
+# unit = 2
 
-ftup = (patient, dset, chan, unit)
+# ftup = (patient, dset, chan, unit)
 
-ddir = '/home/jmp33/data/bartc/plexdata/'
-pdir = 'patient' + str(ftup[0]).zfill(3)
-fname = ('times_' + str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + 
-    str(ftup[2]) + '.mat')
-fullname = ddir + pdir + '/' + fname
 
-dat = sio.loadmat(fullname)['cluster_class']
-unit = dat[:,0].astype('int')
-times = dat[:,1]/1000. #times are originally in ms
-sortord = np.argsort(times) #spikes aren't always time-sorted
-times = times[sortord]
-unit = unit[sortord]
-ddict = {'patient': ftup[0], 'dataset': ftup[1], 
-'channel': ftup[2], 'unit': unit, 'time': times}
-df = pd.DataFrame(ddict)
+if __name__ == '__main__':
 
-db = MySQLdb.connect(host='localhost',
-    user='root', passwd='', db='bartc')
-pdsql.write_frame(df, con=db, name='spikes',
-    if_exists='append', flavor='mysql')
+    # build database and tables
+    SetupDB()
+
+    # locations of relevant files
+    ddir = '/home/jmp33/data/bartc/plexdata/'
+    spkfile = '/home/jmp33/code/hephys/valid_units.csv'
+
+    # get list of tuples with valid channels
+    ulist = []
+
+    with open(spkfile) as infile:
+        for line in infile:
+            ulist.append(tuple(map(int, line.split(','))))
+
+    # iterate through files, loading data
+    for ftup in ulist:
+        ImportSpikes(ftup, ddir)
+
