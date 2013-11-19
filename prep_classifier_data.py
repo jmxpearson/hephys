e3054b731e5279a7393ef8f3a0372c8a34770cbe
John Pearson <jmxpearson@gmail.com>
2013-11-18 20:39:37 -0500

    data prep for lfp glm is working but not yet looping

--- prep_classifier_data.py
+++ prep_classifier_data.py
@@ -1,7 +1,5 @@
 import numpy as np
-import MySQLdb
 import pandas as pd
-import pandas.io.sql as pdsql
 import scipy.signal as ssig
 from physutils import *
 
@@ -34,21 +32,27 @@ tindex = np.arange(0, allbands.index[-1], dt)
 parts = [pd.DataFrame(decimate(aa[1], decfrac)) for aa in allbands.iteritems()]
 allbands = pd.concat(parts, axis=1)
 allbands.index = tindex
+allbands.index.name = 'time'
 allbands.columns = filters
+
+# get instantaneous power
+allbands = allbands.apply(ssig.hilbert)
+allbands = allbands.apply(np.abs)
 
 # handle censoring
 excludes = getCensor(*dtup)
 excludes = excludes[::decfrac]
 allbands[excludes] = np.nan
 
-# grab events (successful stops: true positives for training)
+# grab events (successful stops = true positives for training)
 evt = getEvent('banked', *dtup[:-1])
 # extend with nearby times
-evt = pd.DataFrame(pd.concat([evt, evt - 0.5, evt - 1.0]), columns=['time'])
-evt['outcome'] = 1
+truepos = (pd.DataFrame(pd.concat([evt, evt - 0.5, evt - 1.0]), 
+    columns=['time']))
+truepos['outcome'] = 1
 
 # specify peri-event times
-Tpre = -2  # time relative to event to start
+Tpre = 2  # time relative to event to start
 Tpost = 1.5  # time following event to exclude
 
 # grab random timepoints (true negatives in training set)
@@ -56,15 +60,15 @@ Nrand = 1000
 maxT = np.max(allbands.index.values)
 rand_times = np.array([])
 while len(rand_times) < Nrand:
-    # generate a candidate time point with at least abs(Tpre) before to grab
-    tt = np.random.rand() * (maxT + Tpre) - Tpre
+    # generate a candidate time point with at least Tpre before to grab
+    tt = np.random.rand() * (maxT - Tpre) + Tpre
 
     # how close are we to already selected times
     dist = evt - tt
     #rdist = np.abs(rand_times - tt)
 
     # if dist in [Tpre,0] or [0, Tpost], reject
-    if (np.any(np.logical_and(dist < -Tpre, dist > 0)) or 
+    if (np.any(np.logical_and(dist < Tpre, dist > 0)) or 
         np.any(np.logical_and(dist > -Tpost, dist < 0))): 
         pass
     else:
@@ -73,4 +77,16 @@ trueneg = pd.DataFrame(rand_times, columns=['time'])
 trueneg['outcome'] = 0
 
 # concatenate all training events
-evt = pd.concat([evt, trueneg])
+allevt = pd.concat([truepos, trueneg])
+allevt['time'] = np.around(allevt['time'] / dt) * dt
+allevt = allevt.set_index('time')
+
+# get running average estimate of power at each timepoint of interest
+meanpwr = pd.rolling_mean(allbands, np.ceil(Tpre / dt), min_periods=1)
+tset = pd.concat([meanpwr, allevt], axis=1, join='inner')
+
+# write out
+outdir = '/home/jmp33/data/bartc/'
+outfile = outdir + str(dtup[0]) + '.' + str(dtup[1]) + '.lfpglmdata.csv'
+
+tset.to_csv(outfile)
