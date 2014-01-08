c772da9f00d3a330446f47e3e53ed16718723cb8
John Pearson <jmxpearson@gmail.com>
2014-01-08 10:55:14 -0500

    basic lfp dataset functionality working

--- lfpset.py
+++ lfpset.py
@@ -1,6 +1,7 @@
 """
 LFPset.py
 LFPset is a wrapper class for a Pandas dataframe object containing LFP data.
+Implements methods from physutils.py.
 
 Methods:
 decimate
@@ -12,9 +13,24 @@ split
 As per Pandas convention, these should return new dataframes.
 """
 
-class LFPset:
+import physutils
+
+class LFPset(object):
     def __init__(self, dataframe):
         self.dataframe = dataframe
 
     def __getattr__(self, name):
         return getattr(self.dataframe, name)
+
+    def __str__(self):
+        return self.dataframe.__str__()
+
+    def __repr__(self):
+        return 'LFP dataset object containing a\n' + self.dataframe.__repr__()
+
+    def decimate(self, decfrac):
+        return LFPset(physutils.dfdecimate(self.dataframe, decfrac))
+
+    def bandlimit(self, *args):
+        newdf = physutils.bandlimit(self.dataframe, *args)
+        return LFPset(newdf)
