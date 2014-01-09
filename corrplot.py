b95a3efa499d39f1e2701d638e7799c54dcc7593
John Pearson <jmxpearson@gmail.com>
2014-01-09 13:59:21 -0500

    functioning dendrogram plot of overall channel similarity

--- corrplot.py
+++ corrplot.py
@@ -3,6 +3,8 @@ gist to plot cross-channel LFP correlations as a function of time
 """
 from physutils import *
 from physclasses import * 
+import numpy as np
+import matplotlib.pyplot as plt
 
 # pick a dataset
 dtup = 17, 2 
@@ -10,8 +12,20 @@ dtup = 17, 2
 # open data file
 dbname = '/home/jmp33/data/bartc/plexdata/bartc.hdf5'
 
-# get lfp data
+# get lfp data, normalize, downsample
 df = fetch_all_such_LFP(dbname, *dtup)
+df = df.zscore()
 df = df.decimate(5)
     
-# step through time, take chunks, compute cross-correlation matrix
+# first, try to just look at overall correlations
+# if the similarity matrix is correlation, the corresponding distance 
+# measure is indeed mean-squared distance 
+from scipy.spatial.distance import pdist
+# have to transpose because pdist expects each ROW an observation:
+Dmat = pdist(df.values.T)  
+import scipy.cluster.hierarchy as clust
+dendro = clust.linkage(Dmat)
+
+#plot it:
+tree = clust.dendrogram(dendro)
+plt.show()
