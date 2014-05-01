0c007dd7715790156600edb7cfb2677d392e481c
John Pearson <jmxpearson@gmail.com>
2014-05-01 15:25:44 -0400

    data import process now happens through DataSets class
    all prior functions are now methods, and the load_all method calls
    methods to import events, censoring, spikes, and lfp

--- build_db.py
+++ build_db.py
@@ -4,168 +4,255 @@ import scipy.io as sio
 import numpy as np
 from physutils import make_path, decimate
 
-def write_to_db(dbname, tblname, df, **kwargs):
-    df.to_hdf(dbname, tblname, append=True)
+class DataSets:
+    def __init__(self, data_dir, behavior_dir, channel_file, 
+        spk_file, lfp_file, behavior_file, output_file):
+        self.datadir = data_dir  # directory for plexon data files
+        self.behdir = behavior_dir  # directory for behavior files
+        self.chanfile = channel_file  # file listing spike channels to import
+        self.spkfile = spk_file  # file listing spike units to import
+        self.lfpfile = lfp_file  # file listing lfp channels to import
+        self.behfile = behavior_file  # file mapping phys to behavior files
+        self.outfile = output_file  # name of output data file
 
-def add_metadata(dbname, tblname, **kwargs):
-    fobj = h5py.File(dbname, 'a')
-    for k in kwargs:
-        fobj[tblname].attrs[k] = kwargs[k]
-    fobj.close()
+    def write_to_db(self, tblname, df, **kwargs):
+        df.to_hdf(self.outfile, tblname, append=True)
 
-def import_spikes(ftup, datadir, outfile):
-    pdir = 'patient' + str(ftup[0]).zfill(3)
-    fname = ('times_' + str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + 
-        str(ftup[2]) + '.mat')
-    fullname = datadir + pdir + '/' + fname
-    dat = sio.loadmat(fullname)['cluster_class']
-    
-    unit = dat[:,0].astype('int')
-    times = np.around(dat[:,1]/1000., decimals=3) #times are originally in ms
-    sortord = np.argsort(times) #spikes aren't always time-sorted
-    times = times[sortord]
-    unit = unit[sortord]
-    
-    # now restrict to valid units:
-    valid = unit == ftup[3]
-    times = times[valid]
-    unit = unit[valid]
+    def add_metadata(self, dbname, tblname, **kwargs):
+        fobj = h5py.File(dbname, 'a')
+        for k in kwargs:
+            fobj[tblname].attrs[k] = kwargs[k]
+        fobj.close()
 
-    ddict = {'patient': ftup[0], 'dataset': ftup[1], 
-    'channel': ftup[2], 'unit': unit, 'time': times}
-    df = pd.DataFrame(ddict)
+    def import_spikes(self, ftup):
+        pdir = 'patient' + str(ftup[0]).zfill(3)
+        fname = ('times_' + str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + 
+            str(ftup[2]) + '.mat')
+        fullname = self.datadir + pdir + '/' + fname
+        dat = sio.loadmat(fullname)['cluster_class']
+        
+        unit = dat[:,0].astype('int')
+        times = np.around(dat[:,1]/1000., decimals=3) #times are originally in ms
+        sortord = np.argsort(times) #spikes aren't always time-sorted
+        times = times[sortord]
+        unit = unit[sortord]
+        
+        # now restrict to valid units:
+        valid = unit == ftup[3]
+        times = times[valid]
+        unit = unit[valid]
 
-    target = 'spikes/' + make_path(*ftup)
-    write_to_db(datadir + outfile, target, df)
-
-def import_lfp(ftup, datadir, outfile):
-    pdir = 'patient' + str(ftup[0]).zfill(3)
-    fname = str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + str(ftup[2]) + '.mat'
-    fullname = datadir + pdir + '/' + fname
-    dset = h5py.File(fullname, 'r')
-    dat = dset['data'].value.squeeze()
-    sr = dset['srlfp'].value.squeeze()
-
-    vv = dat
-    decfrac = 5.0
-    vv = decimate(dat, decfrac)  # decimate data to 200 Hz
-    sr = sr / decfrac;
-    dt = (1. / sr).round(3)
-
-    times = (np.arange(0, vv.size) * dt).round(3).squeeze()
-    ddict = {'patient': ftup[0], 'dataset': ftup[1], 
-    'channel': ftup[2], 'time': times, 'voltage': vv}
-
-    df = pd.DataFrame(ddict)
- 
-    target = 'lfp/' + make_path(*ftup) 
-    write_to_db(datadir + outfile, target, df)
-
-def import_censor(ftup, datadir, outfile):
-    pdir = 'patient' + str(ftup[0]).zfill(3)
-    fname = (str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + str(ftup[2]) + 
-        '_censoring.mat')
-    fullname = datadir + pdir + '/' + fname
-    excludes = sio.loadmat(fullname)['excludes'].round(3)
-
-    if excludes.size != 0:
-        #do this in case some exclude ranges make no sense
-        badrng = np.where(np.diff(excludes, axis=1) < 0)
-        excludes = np.delete(excludes, badrng, axis=0)
-
-        # get data ready
         ddict = {'patient': ftup[0], 'dataset': ftup[1], 
-        'channel': ftup[2], 'start': excludes[:,0], 'stop': excludes[:,1]}
+        'channel': ftup[2], 'unit': unit, 'time': times}
+        df = pd.DataFrame(ddict)
+
+        target = 'spikes/' + make_path(*ftup)
+        self.write_to_db(target, df)
+
+    def import_lfp(self, ftup):
+        pdir = 'patient' + str(ftup[0]).zfill(3)
+        fname = str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + str(ftup[2]) + '.mat'
+        fullname = self.datadir + pdir + '/' + fname
+        dset = h5py.File(fullname, 'r')
+        dat = dset['data'].value.squeeze()
+        sr = dset['srlfp'].value.squeeze()
+
+        vv = dat
+        decfrac = 5.0
+        vv = decimate(dat, decfrac)  # decimate data to 200 Hz
+        sr = sr / decfrac;
+        dt = (1. / sr).round(3)
+
+        times = (np.arange(0, vv.size) * dt).round(3).squeeze()
+        ddict = {'patient': ftup[0], 'dataset': ftup[1], 
+        'channel': ftup[2], 'time': times, 'voltage': vv}
 
         df = pd.DataFrame(ddict)
      
-        target = 'censor/' + make_path(*ftup) 
-        write_to_db(datadir + outfile, target, df)
+        target = 'lfp/' + make_path(*ftup) 
+        self.write_to_db(target, df)
 
-def import_events(ftup, datadir, behdir, outfile):
-    pdir = 'patient' + str(ftup[0]).zfill(3)
-    fname = str(ftup[0]) + '.' + str(ftup[1]) + '.plx_events.mat'
-    fullname = datadir + pdir + '/' + fname
-    behname = behdir + pdir + '/' + ftup[2]
+    def import_censor(self, ftup):
+        pdir = 'patient' + str(ftup[0]).zfill(3)
+        fname = (str(ftup[0]) + '.' + str(ftup[1]) + '.plx' + str(ftup[2]) + 
+            '_censoring.mat')
+        fullname = self.datadir + pdir + '/' + fname
+        excludes = sio.loadmat(fullname)['excludes'].round(3)
 
-    # load plexon events
-    evt = sio.loadmat(fullname)['evt'].squeeze()
+        if excludes.size != 0:
+            #do this in case some exclude ranges make no sense
+            badrng = np.where(np.diff(excludes, axis=1) < 0)
+            excludes = np.delete(excludes, badrng, axis=0)
 
-    # load matlab events
-    matevt = sio.loadmat(behname, squeeze_me = True)['data']
+            # get data ready
+            ddict = {'patient': ftup[0], 'dataset': ftup[1], 
+            'channel': ftup[2], 'start': excludes[:,0], 'stop': excludes[:,1]}
 
-    # was this an FHC recording? if so, there are no Plexon stamps
-    isFHC = (evt[0].size < 10)
+            df = pd.DataFrame(ddict)
+         
+            target = 'censor/' + make_path(*ftup) 
+            self.write_to_db(target, df)
 
-    # make a dataframe from matlab behavior, pull out event codes
-    bf = pd.DataFrame(matevt)
-    bf.index.names = ['trial']
-    evf = bf[['ev', 'evt']]
-    bf = bf.drop(['ev', 'evt'], axis=1)
-    # make sure to get data types right
-    bf = bf.convert_objects()
-    bf['result'] = bf['result'].apply(str)
-    absstart = bf['trial_start_time'][0]
+    def import_events(self, ftup):
+        pdir = 'patient' + str(ftup[0]).zfill(3)
+        fname = str(ftup[0]) + '.' + str(ftup[1]) + '.plx_events.mat'
+        fullname = self.datadir + pdir + '/' + fname
+        behname = self.behdir + pdir + '/' + ftup[2]
 
-    # turn each trial into a dataframe 
-    tlist = []
-    for ind in evf.index:
-        thistrial = {'event': evf['ev'][ind], 'time': evf['evt'][ind]}
-        miniframe = pd.DataFrame(thistrial, 
-            index=pd.Index(ind * np.ones_like(evf['ev'][ind]), name='trial'))
-        # now take care of timestamps by putting times in seconds and adjusting
-        # by start time of first trial
-        miniframe['time'] /= 1000. 
-        miniframe['time'] += bf['trial_start_time'][ind] - absstart
-        miniframe['time'] = miniframe['time'].round(3)  # round to ms
-        tlist.append(miniframe)
+        # load plexon events
+        evt = sio.loadmat(fullname)['evt'].squeeze()
 
-    # concatenate trials
-    events = pd.concat(tlist)
-    # make event names column names
-    events = events.set_index('event', append=True).unstack()
-    # get rid of multi-index labeling
-    events.columns = pd.Index([e[1] for e in events.columns])
+        # load matlab events
+        matevt = sio.loadmat(behname, squeeze_me=True)['data']
 
-    # now merge both event datasets 
-    df = pd.concat([bf, events], axis=1)
-    df['patient'] = ftup[0]
-    df['dataset'] = ftup[1]
+        # was this an FHC recording? if so, there are no Plexon stamps
+        isFHC = (evt[0].size < 10)
 
-    # if we have Plexon events, use them
-    if not isFHC:
-        # trial start -- sometimes a spurious event marks recording onset
-        if df.shape[0] == evt[0].shape[0]:  # same number of trial starts 
-            df['trial_start'] = evt[0].round(3).squeeze()
+        # make a dataframe from matlab behavior, pull out event codes
+        bf = pd.DataFrame(matevt)
+        bf.index.names = ['trial']
+        evf = bf[['ev', 'evt']]
+        bf = bf.drop(['ev', 'evt'], axis=1)
+        # make sure to get data types right
+        bf = bf.convert_objects()
+        bf['result'] = bf['result'].apply(str)
+        absstart = bf['trial_start_time'][0]
+
+        # turn each trial into a dataframe 
+        tlist = []
+        for ind in evf.index:
+            thistrial = {'event': evf['ev'][ind], 'time': evf['evt'][ind]}
+            miniframe = pd.DataFrame(thistrial, 
+                index=pd.Index(ind * np.ones_like(evf['ev'][ind]), name='trial'))
+            # now take care of timestamps by putting times in seconds and adjusting
+            # by start time of first trial
+            miniframe['time'] /= 1000. 
+            miniframe['time'] += bf['trial_start_time'][ind] - absstart
+            miniframe['time'] = miniframe['time'].round(3)  # round to ms
+            tlist.append(miniframe)
+
+        # concatenate trials
+        events = pd.concat(tlist)
+        # make event names column names
+        events = events.set_index('event', append=True).unstack()
+        # get rid of multi-index labeling
+        events.columns = pd.Index([e[1] for e in events.columns])
+
+        # now merge both event datasets 
+        df = pd.concat([bf, events], axis=1)
+        df['patient'] = ftup[0]
+        df['dataset'] = ftup[1]
+
+        # if we have Plexon events, use them
+        if not isFHC:
+            # trial start -- sometimes a spurious event marks recording onset
+            if df.shape[0] == evt[0].shape[0]:  # same number of trial starts 
+                df['trial_start'] = evt[0].round(3).squeeze()
+            else:
+                df['trial_start'] = evt[0][1:].round(3).squeeze()
+
+            # trial stop -- when last trial aborted, may not be present
+            if df.shape[0] == evt[7].shape[0]:  # same number of trial starts 
+                df['trial_over'] = evt[7].round(3).squeeze()
+            else:
+                df['trial_over'][:-1] = evt[7].round(3).squeeze()
+
+            # rest of vars
+            vlist = zip(['start inflating', 'stop inflating', 'banked', 'outcome', 
+                'popped'], [1, 2, 3, 5, 4])
+            for var in vlist:
+                valid = pd.notnull(df[var[0]])
+                df[var[0]][valid] = evt[var[1]].round(3).squeeze()
+
+        # lastly, if we are missing control columns, make sure to add them
+        # (important for getting schema correct on initial write)
+        if not 'is_control' in df.keys():
+            df['is_control'] = np.nan
         else:
-            df['trial_start'] = evt[0][1:].round(3).squeeze()
+            df['is_control'] = df['is_control'].astype('float64')
+        if not 'ctrltime' in df.keys():
+            df['ctrltime'] = np.nan
+        
+        # do some final tidying
+        df = df[df['result'] != 'aborted']  # get rid of aborts
+        target = 'events/' + make_path(*ftup[:-1]) 
+        self.write_to_db(target, df)
 
-        # trial stop -- when last trial aborted, may not be present
-        if df.shape[0] == evt[7].shape[0]:  # same number of trial starts 
-            df['trial_over'] = evt[7].round(3).squeeze()
-        else:
-            df['trial_over'][:-1] = evt[7].round(3).squeeze()
+    def load_all_events(self):
+        # get all (patient, dataset) tuples from already loaded spikes
+        tuplist = []
+        print 'Loading Events....'
+        with open(behfile) as infile:
+            for line in infile:
+                thistup = tuple(line.rstrip().lower().split(','))
+                tuplist.append((int(thistup[0]), int(thistup[1]), thistup[2]))
 
-        # rest of vars
-        vlist = zip(['start inflating', 'stop inflating', 'banked', 'outcome', 
-            'popped'], [1, 2, 3, 5, 4])
-        for var in vlist:
-            valid = pd.notnull(df[var[0]])
-            df[var[0]][valid] = evt[var[1]].round(3).squeeze()
+        plist = [t[:-1] for t in tuplist]
+        dset.write_to_db('meta/evlist', 
+            pd.DataFrame(plist, columns=['patient', 'dataset']))
 
-    # lastly, if we are missing control columns, make sure to add them
-    # (important for getting schema correct on initial write)
-    if not 'is_control' in df.keys():
-        df['is_control'] = np.nan
-    else:
-        df['is_control'] = df['is_control'].astype('float64')
-    if not 'ctrltime' in df.keys():
-        df['ctrltime'] = np.nan
-    
-    # do some final tidying
-    df = df[df['result'] != 'aborted']  # get rid of aborts
-    target = 'events/' + make_path(*ftup[:-1]) 
-    write_to_db(datadir + outfile, target, df)
+        for ftup in tuplist:
+            print ftup
+            dset.import_events(ftup)
+
+    def load_all_censoring(self):
+        # load censoring data
+        tuplist = []
+        print 'Loading Censoring data....'
+        with open(chanfile) as infile:
+            for line in infile:
+                tuplist.append(tuple(map(int, line.split(','))))
+        with open(lfpfile) as infile:
+            for line in infile:
+                tuplist.append(tuple(map(int, line.split(','))))
+
+        dset.write_to_db('meta/censlist', 
+            pd.DataFrame(tuplist, columns=['patient', 'dataset', 'channel']))
+
+        # iterate through files, loading data
+        for ftup in tuplist:
+            print ftup
+            dset.import_censor(ftup)
+
+    def load_all_spikes(self):
+        # get list of tuples with valid channels
+        tuplist = []
+        print 'Loading Spikes....'
+        with open(spkfile) as infile:
+            for line in infile:
+                tuplist.append(tuple(map(int, line.split(','))))
+
+        dset.write_to_db('meta/spklist', 
+            pd.DataFrame(tuplist, 
+                columns=['patient', 'dataset', 'channel', 'unit']))
+
+        # iterate through files, loading data
+        for ftup in tuplist:
+            print ftup
+            dset.import_spikes(ftup)
+
+    def load_all_lfp(self):
+        # load lfp data
+        tuplist = []
+        print 'Loading LFP....'
+        with open(lfpfile) as infile:
+            for line in infile:
+                tuplist.append(tuple(map(int, line.split(','))))
+
+        dset.write_to_db('meta/lfplist', 
+            pd.DataFrame(tuplist, columns=['patient', 'dataset', 'channel']))
+
+        # iterate through files, loading data
+        for ftup in tuplist:
+            print ftup
+            dset.import_lfp(ftup)
+
+    def load_all(self):
+        self.load_all_events()
+        self.load_all_censoring()
+        self.load_all_spikes()
+        self.load_all_lfp()
+
 
 
 if __name__ == '__main__':
@@ -173,77 +260,12 @@ if __name__ == '__main__':
     # locations of relevant files
     ddir = '/home/jmp33/data/bartc/plexdata/'
     bdir = '/home/jmp33/data/bartc/behavior/'
+    chanfile = '/home/jmp33/code/hephys/valid_channels.csv'
     spkfile = '/home/jmp33/code/hephys/valid_units.csv'
     lfpfile = '/home/jmp33/code/hephys/lfp_channel_file.csv'
-    chanfile = '/home/jmp33/code/hephys/valid_channels.csv'
     behfile = '/home/jmp33/code/hephys/behavior_file_map.csv'
-    outfile = 'bartc.hdf5'
+    outfile = '/home/jmp33/data/bartc/plexdata/bartc.hdf5'
 
-    ############### events #################        
-    # get all (patient, dataset) tuples from already loaded spikes
-    tuplist = []
-    print 'Loading Events....'
-    with open(behfile) as infile:
-        for line in infile:
-            thistup = tuple(line.rstrip().lower().split(','))
-            tuplist.append((int(thistup[0]), int(thistup[1]), thistup[2]))
+    dset = DataSets(ddir, bdir, chanfile, spkfile, lfpfile, behfile, outfile)
 
-    plist = [t[:-1] for t in tuplist]
-    write_to_db(ddir + 'bartc.hdf5', 'meta/evlist', 
-        pd.DataFrame(plist, columns=['patient', 'dataset']))
-
-    for ftup in tuplist:
-        print ftup
-        import_events(ftup, ddir, bdir, outfile)
-
-    ############### censoring #################
-    # load censoring data
-    tuplist = []
-    print 'Loading Censoring data....'
-    with open(chanfile) as infile:
-        for line in infile:
-            tuplist.append(tuple(map(int, line.split(','))))
-    with open(lfpfile) as infile:
-        for line in infile:
-            tuplist.append(tuple(map(int, line.split(','))))
-
-    write_to_db(ddir + outfile, 'meta/censlist', 
-        pd.DataFrame(tuplist, columns=['patient', 'dataset', 'channel']))
-
-    # iterate through files, loading data
-    for ftup in tuplist:
-        print ftup
-        import_censor(ftup, ddir, outfile)
-
-    ############### spikes #################
-    # get list of tuples with valid channels
-    tuplist = []
-    print 'Loading Spikes....'
-    with open(spkfile) as infile:
-        for line in infile:
-            tuplist.append(tuple(map(int, line.split(','))))
-
-    write_to_db(ddir + outfile, 'meta/spklist', 
-        pd.DataFrame(tuplist, 
-            columns=['patient', 'dataset', 'channel', 'unit']))
-
-    # iterate through files, loading data
-    for ftup in tuplist:
-        print ftup
-        import_spikes(ftup, ddir, outfile)
-
-    ############### lfp #################
-    # load lfp data
-    tuplist = []
-    print 'Loading LFP....'
-    with open(lfpfile) as infile:
-        for line in infile:
-            tuplist.append(tuple(map(int, line.split(','))))
-
-    write_to_db(ddir + outfile, 'meta/lfplist', 
-        pd.DataFrame(tuplist, columns=['patient', 'dataset', 'channel']))
-
-    # iterate through files, loading data
-    for ftup in tuplist:
-        print ftup
-        import_lfp(ftup, ddir, outfile)
+    dset.load_all()
