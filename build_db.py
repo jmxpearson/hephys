a77c2a5a80f8eb0ee7ceb4abf663ea1783aafce5
John Pearson <jmxpearson@gmail.com>
2014-05-07 11:51:00 -0400

    changed csv locations

--- build_db.py
+++ build_db.py
@@ -263,10 +263,10 @@ if __name__ == '__main__':
     # locations of relevant files
     ddir = '/home/jmp33/data/bartc/plexdata/'
     bdir = '/home/jmp33/data/bartc/behavior/'
-    chanfile = '/home/jmp33/code/hephys/valid_channels.csv'
-    spkfile = '/home/jmp33/code/hephys/valid_units.csv'
-    lfpfile = '/home/jmp33/code/hephys/lfp_channel_file.csv'
-    behfile = '/home/jmp33/code/hephys/behavior_file_map.csv'
+    chanfile = '/home/jmp33/code/hephys/bartc/valid_channels.csv'
+    spkfile = '/home/jmp33/code/hephys/bartc/valid_units.csv'
+    lfpfile = '/home/jmp33/code/hephys/bartc/lfp_channel_file.csv'
+    behfile = '/home/jmp33/code/hephys/bartc/behavior_file_map.csv'
     outfile = '/home/jmp33/data/bartc/plexdata/bartc.hdf5'
 
     plexon_event_codes = {'start inflating': 1, 'stop inflating': 2, 'banked': 3, 'outcome': 5, 'popped': 4, 'trial_start': 0, 'trial_over': 7}
