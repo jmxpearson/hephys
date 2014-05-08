ec87df2e2e7ab983301282e4779b2edf2b6723df
John Pearson <jmxpearson@gmail.com>
2014-05-08 10:56:17 -0400

    refactor psth plot into function

--- gonogo_scratch.py
+++ gonogo_scratch.py
@@ -1,24 +1,28 @@
 from physutils import *
 from pylab import *
 
-# get all spikes for a given unit 
-dbname = '/home/jmp33/data/gonogo/plexdata/gonogo.hdf5'
-patient = 32
-dataset = 1
-channel = 1
-unit = 1
-df = fetch(dbname, 'spikes', patient, dataset, channel, unit)
-evt = fetch(dbname, 'events', patient, dataset)
+def plot_psth(dbname, ftup, evt_name):
+    df = fetch(dbname, 'spikes', *ftup) 
+    evt = fetch(dbname, 'events', *ftup[:2])
 
-binsize = 0.050  # 50 ms bin
-binned = binspikes(df, binsize)
+    binsize = 0.050  # 50 ms bin
+    binned = binspikes(df, binsize)
 
-event_name = 'responded'
-evt_set = evt[evt['event'] == event_name]['time']
+    evt_set = evt[evt['event'] == evt_name]['time']
 
-psth = evtsplit(binned, evt_set, -1, 1).mean(axis=1)
+    psth = evtsplit(binned, evt_set, -0.5, 1).mean(axis=1) / binsize
 
-smpsth = smooth(psth, 0.4)
+    smpsth = smooth(psth, 0.4)
 
-plot(smpsth.index, smpsth)
-show()
+    plot(smpsth.index, smpsth)
+    show()
+
+if __name__ == '__main__':
+    dbname = '/home/jmp33/data/gonogo/plexdata/gonogo.hdf5'
+    patient = 33 
+    dataset = 2
+    channel = 1
+    unit = 1
+    evt_name = 'no_response'
+
+    plot_psth(dbname, (patient, dataset, channel, unit), evt_name)
