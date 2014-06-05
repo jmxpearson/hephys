768d1079add85c0d68a754441c32e41601ec38c7
John Pearson <jmxpearson@gmail.com>
2014-06-05 11:05:03 -0400

    sanity-preserving directory reorg

--- gonogo/build_gonogo_db.py
+++ gonogo/build_gonogo_db.py
@@ -3,10 +3,10 @@ from build_db import *
 # locations of relevant files
 ddir = '/home/jmp33/data/gonogo/plexdata/'
 bdir = '/home/jmp33/data/gonogo/behavior/'
-chanfile = '/home/jmp33/code/hephys/gonogo/valid_channels.csv'
-spkfile = '/home/jmp33/code/hephys/gonogo/valid_units.csv'
-lfpfile = '/home/jmp33/code/hephys/gonogo/lfp_channel_file.csv'
-behfile = '/home/jmp33/code/hephys/gonogo/behavior_file_map.csv'
+chanfile = '/home/jmp33/code/hephys/gonogo/data/valid_channels.csv'
+spkfile = '/home/jmp33/code/hephys/gonogo/data/valid_units.csv'
+lfpfile = '/home/jmp33/code/hephys/gonogo/data/lfp_channel_file.csv'
+behfile = '/home/jmp33/code/hephys/gonogo/data/behavior_file_map.csv'
 outfile = '/home/jmp33/data/gonogo/plexdata/gonogo.hdf5'
 
 plexon_event_codes = {}
