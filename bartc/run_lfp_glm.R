d95805c0193c0492288dd0f8dc6c3ee6de0dec47
John Pearson <jmxpearson@gmail.com>
2014-06-17 11:35:08 -0400

    run_lfp_glm.R now does grid search over values of elastic net alpha
    alpha = 0:0.1:1 are used
    caret is used to parition data for cross-validation
    returned object for each dataset includes alpha, glmnet object
    pertaining to that alpha, betas from the fit

--- bartc/run_lfp_glm.R
+++ bartc/run_lfp_glm.R
@@ -2,22 +2,73 @@
 run_lfp_glm <- function(dfile){
   # given an input data file, perform an elastic net regression
   # on the data contained therein
+
   dat <- read.table(dfile, sep=',', header=TRUE, row.names=1, colClasses=c('numeric'))
   
   # make model matrix
   y <- dat[, 1]
   X <- dat[, -1]
+
+  allobjs = list()
+  alphalist = seq(0, 1, 0.1)
+
+  folds <- partition_data(y, nfolds=10)
   
-  # run sparse regression
-  # don't include constant in X; glmnet will fit an intercept, but if we supply
-  # one manually, it will be penalized, which we don't want
-  glmobj <- cv.glmnet(as.matrix(X), y, family = 'binomial', 
-                      nfolds=10, intercept=TRUE, type.measure="auc")
-  plot(glmobj)
-  fit <- glmobj$glmnet.fit
+  # loop over alpha values
+  for (alpha in alphalist) {
+    # run sparse regression
+    # don't include constant in X; glmnet will fit an intercept, 
+    # but if we supply
+    # one manually, it will be penalized, which we don't want
+    glmobj <- cv.glmnet(as.matrix(X), y, alpha = alpha, family = 'binomial', 
+                        foldid = folds, intercept = TRUE, type.measure = "auc")
+
+    allobjs[[length(allobjs) + 1]] <- get_best_beta(glmobj)
+  }
+
+  bestobj <- get_best_alpha(allobjs, alphalist)
+
+  plot(bestobj$glmobj)
+  fit <- bestobj$glmobj$glmnet.fit
   plot(fit, xvar='lambda', label=TRUE)
-  minlambda <- glmobj$lambda.min
+
+  return(bestobj)
+}
+
+partition_data <- function(outcome, nfolds) {
+  require('caret')
+
+  folds <- createFolds(outcome, nfolds)
+
+  foldid <- rep(NA, length(outcome))
+
+  for (ind in 1:nfolds) {
+    foldid[folds[[ind]]] <- ind
+  }
+
+  return(foldid)
+}
+
+get_best_beta <- function(glmobj) {
+  # given a glmobj from elastic net, extract the cross-validation score and
+  # coefficients of the best fit
+
+  minlambda <- glmobj$lambda.1se
   minlambda.ind <- which(glmobj$lambda == minlambda)
   beta <- fit$beta[, minlambda.ind]
-  return (list(beta = beta, glmobj = glmobj))
+  score <- glmobj$cvm[minlambda.ind]
+
+  return(list(beta = beta, score = score, glmobj = glmobj))
+}
+
+get_best_alpha <- function(objlist, alphalist) {
+  # given a list of objects returned by get_best_beta and a list of alpha 
+  # values, return the object corresponding to the highest score
+
+  scorelist <- sapply(objlist, function(x) {x$score})
+  max_ind <- which.max(scorelist)  # if using auc, want max score
+
+  bestobj <- c(objlist[[max_ind]], alpha=alphalist[max_ind])
+
+  return(bestobj)
 }
