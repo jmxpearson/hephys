377ab4be69ec8221b6ff9d4d8ce2d3e796974ff5
John Pearson <jmxpearson@gmail.com>
2014-06-23 11:57:18 -0400

    minor name change

--- bartc/correlation_in_time.py
+++ bartc/correlation_in_time.py
@@ -19,7 +19,7 @@ def get_analytic_signal(lfp, filters):
     return lfp.demean().bandlimit(filters).decimate(5).apply(hilbert, 
         raw=True)
 
-def make_correlation_matrix(z):
+def make_correlation_series(z):
     # z is assumed to be a matrix with a channel in each column
     # return matrix is (time, chan, chan)
     zbar = np.expand_dims(np.conj(z), 2)
@@ -29,4 +29,4 @@ def make_correlation_matrix(z):
 name = (17, 2)
 lfp = fetch_all_such_LFP(dbname, *name)
 lfpz = get_analytic_signal(lfp, ['theta']).values
-S = make_correlation_matrix(lfpz)
+S = make_correlation_series(lfpz)
