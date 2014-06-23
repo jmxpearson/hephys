2522b5cd1a3f44ed27187d08c7c3cabef45319db
John Pearson <jmxpearson@gmail.com>
2014-06-23 11:56:43 -0400

    code now makes correlation time series

--- bartc/correlation_in_time.py
+++ bartc/correlation_in_time.py
@@ -15,11 +15,18 @@ setlist = pd.read_hdf(dbname, '/meta/lfplist')
 # group by (patient, dataset) entries:
 groups = setlist.groupby(['patient', 'dataset'])
 
-def process_LFP(lfp, filters):
+def get_analytic_signal(lfp, filters):
     return lfp.demean().bandlimit(filters).decimate(5).apply(hilbert, 
         raw=True)
+
+def make_correlation_matrix(z):
+    # z is assumed to be a matrix with a channel in each column
+    # return matrix is (time, chan, chan)
+    zbar = np.expand_dims(np.conj(z), 2)
+    return np.abs(zbar * np.expand_dims(z, 1))
 
 # for name, grp in groups:
 name = (17, 2)
 lfp = fetch_all_such_LFP(dbname, *name)
-lfpz = process_LFP(lfp, ['theta'])
+lfpz = get_analytic_signal(lfp, ['theta']).values
+S = make_correlation_matrix(lfpz)
