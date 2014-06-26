f8c76fc3fb344c4d99cbf125cebe1af63af6ab05
John Pearson <jmxpearson@gmail.com>
2014-06-26 12:06:36 -0400

    small refactor

--- bartc/correlation_in_time.py
+++ bartc/correlation_in_time.py
@@ -4,18 +4,6 @@ import os
 from physutils import *
 from physclasses import *
 
-# set a random seed
-np.random.seed(12345)
-
-# name of database to use
-dbname = os.path.expanduser('~/data/bartc/plexdata/bartc.hdf5')
-
-# first, get a list of lfp channels
-setlist = pd.read_hdf(dbname, '/meta/lfplist')
-
-# group by (patient, dataset) entries:
-groups = setlist.groupby(['patient', 'dataset'])
-
 def get_eigens(X):
     # assume X is a vector
     numeigs = np.sqrt(X.shape[0])
@@ -101,6 +89,18 @@ def med_then_corr(lfp, events, Tpre, Tpost):
     max_eigen_normed = LFPset(eigseries.div(eigseries.dataframe.iloc[:, -1], axis=0), meta=eigseries.meta.copy())
 
     return (eigseries, total_var_normed, max_eigen_normed)
+
+# set a random seed
+np.random.seed(12345)
+
+# name of database to use
+dbname = os.path.expanduser('~/data/bartc/plexdata/bartc.hdf5')
+
+# first, get a list of lfp channels
+setlist = pd.read_hdf(dbname, '/meta/lfplist')
+
+# group by (patient, dataset) entries:
+groups = setlist.groupby(['patient', 'dataset'])
 
 # for name, grp in groups:
 name = (18, 1)
