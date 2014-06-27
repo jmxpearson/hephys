f0d730de88d64ab67d90618e40cc8fe83d754dd4
John Pearson <jmxpearson@gmail.com>
2014-06-27 11:04:00 -0400

    setting up files for spike glm analysis

--- bartc/run_spk_glm.R
+++ bartc/run_spk_glm.R
@@ -4,13 +4,13 @@ source('setup_env.R')
 
 
 fitobjs = list()
-for (ind in 1:numlfp) {
-  fname <- paste(paste(chanlist[ind,], collapse='.'), 'lfpglmdata.csv', sep='.')
+for (ind in 1:numspk) {
+  fname <- paste(paste(chanlist[ind,], collapse='.'), 'spkglmdata.csv', sep='.')
   dfile <- paste(ddir, fname, sep='/')
   print(dfile)
   dat <- read.table(dfile, sep=',', header=TRUE, row.names=1, colClasses=c('numeric'))
-  thisfit <- run_glm(dat)
+  thisfit <- run_glm(dat, 'poisson')
   fitobjs[[ind]] <- thisfit
 }
 
-save(fitobjs, file=paste(ddir, 'lfpfitdata', sep='/'))
+save(fitobjs, file=paste(ddir, 'spkfitdata', sep='/'))
--- bartc/setup_env.R
+++ bartc/setup_env.R
@@ -2,11 +2,18 @@
 
 adir <- '~/code/hephys/bartc'  # analysis directory
 ddir <- '~/data/bartc'  # data directory
+
+# lfp data
 cfile <- 'data/lfp_channel_file.csv'
 chanlist <- read.csv(paste(adir, cfile, sep='/'), header=FALSE)
 chanlist <- chanlist[, 1:2]
 chanlist <- chanlist[!duplicated(chanlist),]
-numunits <- dim(chanlist)[1]
+numlfp <- dim(chanlist)[1]
+
+# spike data
+cfile <- 'data/valid_units.csv'
+unitlist <- read.csv(paste(adir, cfile, sep='/'), header=FALSE)
+numunits <- dim(unitlist)[1]
 
 library(glmnet)
 library(ggplot2)
