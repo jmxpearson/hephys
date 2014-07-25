8dc45b98b7e16f1afe3ca8906b286bf3d19701cc
John Pearson <jmxpearson@gmail.com>
2014-07-25 16:19:04 -0400

    make_time_frequency_contrast_plots now working

--- bartc/make_time_frequency_contrast_plots.py
+++ bartc/make_time_frequency_contrast_plots.py
@@ -1,31 +1,31 @@
 from matplotlib.backends.backend_pdf import PdfPages
-from physclasses import *
-from physutils import *
+import matplotlib.pyplot as plt
+import pandas as pd
+import physutils
+import hephys.dbio as dbio
 import os
 
 def make_time_frequency_plot(dtup, event_names, Tpre, Tpost, baseline_interval):
 
     # get lfp data
     print "Fetching data: " + str(dtup)
-    lfp = fetch_all_such_LFP(dbname, *dtup).censor()
+    lfp = dbio.fetch_all_such_LFP(dbname, *dtup).censor()
 
     # get events
-    evt = fetch(dbname, 'events', *dtup[:2])
+    evt = dbio.fetch(dbname, 'events', *dtup[:2])
     times0 = evt[event_names[0]].dropna()
     times1 = evt[event_names[1]].dropna()
 
-    contr_tf, fig1 = lfp.contrast_time_frequency(dtup[2], [times0, times1], Tpre, Tpost, method='wav', normfun=norm_by_trial(baseline_interval), 
+    contr_tf, fig1 = lfp.contrast_time_frequency(dtup[2], [times0, times1], Tpre, Tpost, method='wav', normfun=physutils.norm_by_trial(baseline_interval), 
         doplot=True)
 
     mcontr, fig2 = lfp.significant_time_frequency(dtup[2], [times0, times1], Tpre, 
         Tpost, thresh=2.0, niter=1000, method='wav', doplot=True, 
-        normfun=norm_by_trial(baseline_interval))
+        normfun=physutils.norm_by_trial(baseline_interval))
 
     return fig1, fig2
 
 if __name__ == '__main__':
-
-    os.chdir(os.path.expanduser('~/code/hephys/bartc'))
 
     # open data file
     dbname = os.path.expanduser('~/data/bartc/plexdata/bartc.hdf5')
