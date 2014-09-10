b6e7c8ff5c685d2893e01c3a1ab41146e3ded003
John Pearson <jmxpearson@gmail.com>
2014-09-10 14:19:12 -0400

    altered to use log-spaced frequencies

--- bartc/make_time_frequency_plots.py
+++ bartc/make_time_frequency_plots.py
@@ -1,11 +1,12 @@
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import physutils
 import hephys.dbio as dbio
 import os
 
-def make_time_frequency_plot(dbname, dtup, event_name, Tpre, Tpost, baseline_interval):
+def make_time_frequency_plot(dbname, dtup, event_name, Tpre, Tpost, freqs, baseline_interval):
 
     # get lfp data
     print "Fetching data: " + str(dtup)
@@ -33,13 +34,15 @@ if __name__ == '__main__':
     Tpre = -1.5
     Tpost = 0.5
     baseline_interval = (-1.5, -1.35)
+    freqs = np.exp(np.linspace(np.log(2.5), np.log(50)))
 
     # open pdf for plotting
     with PdfPages(os.path.expanduser('~/Dropbox/hephys/media/figs/' + fname)) as pdf:
 
         for idx, channel_inds in setlist.iterrows():
             dtup = tuple(channel_inds)
-            make_time_frequency_plot(dbname, dtup, event_name, Tpre, Tpost, baseline_interval)
+            make_time_frequency_plot(dbname, dtup, event_name, Tpre, Tpost,
+            freqs, baseline_interval)
             titlestr = "Channel: " + str(dtup) + "\nAlign: " + event_name
             plt.title(titlestr)
             pdf.savefig()
